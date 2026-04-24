37e26b1b9ef1403d98fb974be0d42c18dd832925
Adnan <addu87260@gmail.com>
2026-04-24 11:46:38 +0530

    Time: 35 ms (43.37%), Space: 61.5 MB (25.45%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -2,6 +2,18 @@
  * @param {string} s
  * @return {number}
  */
-var firstUniqChar = function(s) {
-    return [...s].findIndex((c) => s.indexOf(c) === s.lastIndexOf(c))
+var firstUniqChar = function (s) {
+    let count = {};
+
+    for (let char of s) {
+        count[char] = (count[char] || 0) + 1;
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        if (count[s[i]] === 1) {
+            return i;
+        }
+    }
+
+    return -1;
 };
