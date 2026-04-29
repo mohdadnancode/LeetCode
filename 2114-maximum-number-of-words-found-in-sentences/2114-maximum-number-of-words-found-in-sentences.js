39b5bbed8ccbdffe52ecc374b4c5670ba57bdd5b
Adnan <addu87260@gmail.com>
2026-04-29 10:08:17 +0530

    Time: 2 ms (74.64%), Space: 55.4 MB (86.74%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -5,15 +5,8 @@
 var mostWordsFound = function (sentences) {
     let max = 0;
     for (let sen of sentences) {
-        let count = 1;
-        for (let i = 0; i < sen.length; i++) {
-            if (sen[i] === " ") {
-                count++;
-            }
-        }
-        if (count > max) {
-            max = count;
-        }
+        let count = sen.split(" ").length;
+        if(count > max) max = count;
     }
     return max;
 };
