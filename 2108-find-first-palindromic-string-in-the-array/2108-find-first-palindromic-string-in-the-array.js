1f8d786cd732b1456b483e76b2aa67bb405fe0f6
Adnan <addu87260@gmail.com>
2026-05-04 11:45:03 +0530

    Time: 14 ms (56.62%), Space: 62.5 MB (48.92%) - LeetHub

---
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string[]} words
+ * @return {string}
+ */
+var firstPalindrome = function (words) {
+    for (let word of words) {
+        let rev = "";
+        for (let i = word.length - 1; i >= 0; i--) {
+            rev += word[i]
+        }
+        if(word === rev) return word;
+    }
+    return "";
+};
