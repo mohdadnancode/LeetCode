2998a3dd152fbb3bce17e545e138c173137097ca
Adnan <addu87260@gmail.com>
2026-05-05 10:23:11 +0530

    Time: 1 ms (10.74%), Space: 53.7 MB (44.52%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.js
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -3,5 +3,16 @@
  * @return {string}
  */
 var toLowerCase = function (s) {
-    return s.toLowerCase();
+    let result = "";
+
+    for(let i = 0; i < s.length; i++){
+        let code = s.charCodeAt(i);
+
+        if(code >= 65 && code <= 90){
+            result += String.fromCharCode(code + 32);
+        } else {
+            result += s[i];
+        }
+    }
+    return result;
 };
