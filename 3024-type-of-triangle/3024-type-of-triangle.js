16dbcb977ae22668955c41173b619d3cea67da6d
Adnan <addu87260@gmail.com>
2026-05-06 10:35:59 +0530

    Time: 0 ms (100%), Space: 54.7 MB (89.19%) - LeetHub

---
+++ 3024-type-of-triangle/3024-type-of-triangle.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {string}
+ */
+var triangleType = function (nums) {
+    const [a, b, c] = nums;
+
+    if(
+        a + b <= c ||
+        a + c <= b ||
+        b + c <= a
+    ) return "none";
+
+    if(
+        a === b &&
+        a === c &&
+        b === c
+    ) return "equilateral";
+
+    if(
+        a === b ||
+        a === c ||
+        b === c
+    ) return "isosceles";
+
+    return "scalene"
+};
