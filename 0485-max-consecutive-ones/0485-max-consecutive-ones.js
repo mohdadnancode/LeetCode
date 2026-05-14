2bdac0f3ac5e804ff7e189d37a1ae24bab202f1d
Adnan <addu87260@gmail.com>
2026-05-14 13:16:56 +0530

    Time: 2 ms (69.34%), Space: 58.7 MB (79.69%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.js
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.js
@@ -6,12 +6,12 @@ var findMaxConsecutiveOnes = function (nums) {
     let count = 0;
     let maxCount = 0;
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] === 1) count++;
+        if (nums[i] === 1) {
+            count++;
+            if (count > maxCount) maxCount = count;
+        }
         else {
             count = 0;
-        }
-        if(count> maxCount){
-            maxCount= count
         }
     }
     return maxCount;
