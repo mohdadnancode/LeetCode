da29ac21a530464786c14b094e81875d67a24b86
Adnan <addu87260@gmail.com>
2026-06-03 11:16:25 +0530

    Time: 47 ms (35.98%), Space: 53.1 MB (84.67%) - LeetHub

--- 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -5,11 +5,12 @@
 var expect = function (val) {
     return {
         toBe: (n) => {
-            if (n === val) return true;
-            else throw new Error("Not Equal");
+            if (val === n) return true;
+            else throw new Error("Not Equal")
         },
+
         notToBe: (n) => {
-            if (n !== val) return true;
+            if (val !== n) return true;
             else throw new Error("Equal");
         }
     }
